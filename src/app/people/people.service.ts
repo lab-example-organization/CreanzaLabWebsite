96bce223b48c5497990a8e14b542252b016bfc68
prundstrom <parker.rundstrom@gmail.com>
2019-02-17 12:23:59 -0600

    import firebaseservice, create class and people-retrieving function

--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { FirebaseService } from '../GlobalServices/firebase.service'
 @Injectable({
   providedIn: 'root'
 })
 
 export class PeopleService {
 
-  constructor() { }
+  constructor(private firebaseserv:FirebaseService) { }
+
+  getPeople(){
+    return this.firebaseserv.returnCollect('people')
+  }
 }
