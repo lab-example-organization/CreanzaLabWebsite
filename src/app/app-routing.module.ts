c250857f490165ae5fb5bfe43e19ac0d46b1f1c4
Jordan <jordan.m.singer@gmail.com>
2019-02-23 10:55:18 -0600

    Add components to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { PeopleComponent } from './people/people.component';
 import { PeopleuploadComponent } from './peopleupload/peopleupload.component';
 import { PublicationsComponent } from './publications/publications.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { IndividualComponent } from './individual/individual.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
-  // {path: '', component: home}
+  {path: '', component: HomeComponent},
   {path: 'people', component: PeopleComponent},
   {path: 'publications', component: PublicationsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'individual', component: IndividualComponent},
+  {path: 'userinfo', component: UserInfoComponent},
   {path: 'peopleupload', component: PeopleuploadComponent}
 ];
 
