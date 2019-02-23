3f1650b371573cce68f855e868a9db991036bf01
Jordan <jordan.m.singer@gmail.com>
2019-02-23 13:32:10 -0600

    Initialize firebase rules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDtsufk7bnOedba6seqMseQYjBPeAmUr2g',
+    authDomain: 'creanza-lab-208216.firebaseapp.com',
+    databaseURL: 'https://creanza-lab-208216.firebaseio.com',
+    projectId: 'creanza-lab-208216',
+    storageBucket: 'creanza-lab-208216.appspot.com',
+    messagingSenderId: '623695311029'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebase:{
-    apiKey: "AIzaSyDtsufk7bnOedba6seqMseQYjBPeAmUr2g",
-    authDomain: "creanza-lab-208216.firebaseapp.com",
-    databaseURL: "https://creanza-lab-208216.firebaseio.com",
-    projectId: "creanza-lab-208216",
-    storageBucket: "creanza-lab-208216.appspot.com",
-    messagingSenderId: "623695311029"
+  firebase: {
+    apiKey: 'AIzaSyDtsufk7bnOedba6seqMseQYjBPeAmUr2g',
+    authDomain: 'creanza-lab-208216.firebaseapp.com',
+    databaseURL: 'https://creanza-lab-208216.firebaseio.com',
+    projectId: 'creanza-lab-208216',
+    storageBucket: 'creanza-lab-208216.appspot.com',
+    messagingSenderId: '623695311029'
   }
 };
 
