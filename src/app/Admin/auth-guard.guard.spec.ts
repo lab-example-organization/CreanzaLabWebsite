90b5065a5a5e95b9184d799408a7ca717b850b70
Jordan Singer <jordan.m.singer@gmail.com>
2019-03-03 13:19:51 -0600

    Fix unit tests (#17)

--- src/app/Admin/auth-guard.guard.spec.ts
+++ src/app/Admin/auth-guard.guard.spec.ts
@@ -1,11 +1,27 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
+const angularFireAuthStub = {
+  authState: new BehaviorSubject({}),
+  auth: {
+      signInWithPopup: jasmine.createSpy('signInWithPopup')
+  },
+};
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [
+        AuthGuard,
+        { provide: AngularFirestore },
+        { provide: AngularFireAuth, useValue: angularFireAuthStub },
+        { provide: Router },
+      ]
     });
   });
 
--- src/app/Admin/auth.service.spec.ts
+++ src/app/Admin/auth.service.spec.ts
@@ -1,9 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
+const angularFireAuthStub = {
+  authState: new BehaviorSubject({}),
+  auth: {
+      signInWithPopup: jasmine.createSpy('signInWithPopup')
+  },
+};
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: AngularFirestore },
+      { provide: AngularFireAuth, useValue: angularFireAuthStub },
+      { provide: Router },
+    ]
+  }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
