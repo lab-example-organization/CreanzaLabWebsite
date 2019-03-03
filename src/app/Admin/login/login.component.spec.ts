47ff6e30705dbc37479d1ea7e1a022516ff06493
unknown <jordan.m.singer@gmail.com>
2019-03-03 13:12:06 -0600

    Fix unit tests

--- src/app/Admin/login/login.component.spec.ts
+++ src/app/Admin/login/login.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+const angularFireAuthStub = {
+  authState: new BehaviorSubject({}),
+  auth: {
+      signInWithPopup: jasmine.createSpy('signInWithPopup')
+  },
+};
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +19,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: AngularFirestore },
+        { provide: AngularFireAuth, useValue: angularFireAuthStub },
+      ]
     })
     .compileComponents();
   }));
