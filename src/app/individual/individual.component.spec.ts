518085b610a26215a434e18bafbc03144e61562e
prundstrom <parker.rundstrom@gmail.com>
2019-03-03 19:53:18 -0600

    added expects for SocialMedia

--- src/app/individual/individual.component.spec.ts
+++ src/app/individual/individual.component.spec.ts
@@ -8,11 +8,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { IndividualComponent } from './individual.component';
 
 import { of } from 'rxjs';
+import { SocialMedia } from '../Classes/socialMedia';
 
 describe('IndividualComponent', () => {
   let component: IndividualComponent;
   let fixture: ComponentFixture<IndividualComponent>;
-  let activatedRoute = new ActivatedRouteStub();
+  const activatedRoute = new ActivatedRouteStub();
 
   const expectedPerson: Person = {
     description: 'This is the description of a person.',
@@ -21,8 +22,33 @@ describe('IndividualComponent', () => {
     project: 'This is the project of a person.',
     startYear: 2017,
     studying: 'This is the studying of a person.',
-    pronouns: 'These are the pronouns of a person',
-    portraitLink: 'This is the portrait link of a person.'
+    pronouns: 'These are the pronouns of a person.',
+    portraitLink: 'This is the portrait link of a person.',
+    socialMedia: 'This is the social media for a person.',
+  };
+
+  const expectedSocialMedia: SocialMedia = {
+    android: 'This is where a person may put their android account.',
+    dribbble: 'This is where a person may put their dribbble account.',
+    facebook: 'This is where a person may put their facebook account.',
+    flickr: 'This is where a person may put their flickr account.',
+    foursquare: 'This is where a person may put their foursquare account.',
+    google: 'This is where a person may put their google account.',
+    instagram: 'This is where a person may put their instagram account.',
+    linkedin: 'This is where a person may put their linkedin account.',
+    pinterest: 'This is where a person may put their pinterest account.',
+    reddit: 'This is where a person may put their reddit account.',
+    rss: 'This is where a person may put their rss account.',
+    skype: 'This is where a person may put their skype account.',
+    snapchat: 'This is where a person may put their snapchat account.',
+    soundcloud: 'This is where a person may put their soundcloud account.',
+    stumbleupon: 'This is where a person may put their stumbleupon account.',
+    tumblr: 'This is where a person may put their tumblr account.',
+    twitter: 'This is where a person may put their twitter account.',
+    vimeo: 'This is where a person may put their vimeo account.',
+    vine: 'This is where a person may put their vine account.',
+    yahoo: 'This is where a person may put their yahoo account.',
+    youtube: 'This is where a person may put their youtube account.',
   };
 
   const data = of(expectedPerson);
