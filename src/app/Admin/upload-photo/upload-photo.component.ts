1cc14a0e7f75acb5232d1fdf70b5a0e51c61fcc4
doraaobodo <46463839+doraaobodo@users.noreply.github.com>
2019-03-09 15:52:09 -0600

    edit upload  photos page

--- src/app/Admin/upload-photo/upload-photo.component.ts
+++ src/app/Admin/upload-photo/upload-photo.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+import { CRUDService } from '../Forms/crud.service';
+//import { UploadPhoto } from '../Classes/UploadPhoto';
+//might need to make a new file
+//import { PeopleService} from './people.service';
+//import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-upload-photo',
@@ -7,9 +14,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadPhotoComponent implements OnInit {
 
-  constructor() { }
+  UploadPhotosForm = this.createForm();
+  message: string;
+  @ViewChild('link') link:ElementRef;
+  eventdata: any;
+
+  constructor(private fb: FormBuilder,
+              private CRUD: CRUDService) { }
 
   ngOnInit() {
   }
 
+  onFile(event:any) {
+    this.eventdata = event
+  }
+  onSubmit() {
+
+
+    let newPhoto = Object.assign({}, this.UploadPhotosForm.value);
+    console.log(newPhoto);
+    this.CRUD.uploadImages([`groupPhotos/${newPhoto.date}${newPhoto.name}`],
+      [this.eventdata]).then(url=>{
+        newPhoto.link = url[0]
+        this.CRUD.uploadItem(newPhoto, 'groupPhotos')
+
+      })
+    .then(() => this.message = "Success!")
+
+  }
+
+  createForm() {
+    return this.fb.group({
+    name: ['', Validators.required],
+    date: ['', Validators.required],
+    caption: ['', Validators.required],
+    link: ['', Validators.required]
+    })
+  }
 }
+
---
+++ src/app/Classes/uploadPhoto.ts
@@ -0,0 +1,7 @@
+export class UploadPhoto {
+    name: string;
+    date: string;
+    caption: string;
+    link: string;
+    
+}
