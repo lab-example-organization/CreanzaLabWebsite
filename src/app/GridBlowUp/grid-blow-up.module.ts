4cbd5d909cfc0ac768b22d883519179128df0b01
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-03-09 15:21:19 -0600

    add grid and blow up module form arecace

---
+++ src/app/GridBlowUp/grid-blow-up.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GridComponent } from '../GridBlowUp/grid/grid.component';
+
+@NgModule({
+  declarations: [GridComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GridBlowUpModule { }
