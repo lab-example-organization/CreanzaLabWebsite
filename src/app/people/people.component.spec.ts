734b7ef8dd276fd5a8f25c3456f4a456084c623d
prundstrom <parker.rundstrom@gmail.com>
2019-03-09 13:23:32 -0600

    added social media

--- src/app/people/people.component.spec.ts
+++ src/app/people/people.component.spec.ts
@@ -39,31 +39,8 @@ describe('PeopleComponent', () => {
       startYear: 2017,
       studying: 'This is the studying of a person.',
       pronouns: 'These are the pronouns of a person',
-      portraitLink: 'This is the portrait link of a person.'
-    },
-    {
-      description: 'This is the description of a person.',
-      endingYear: 'This is the ending year of a person.',
-      name: 'This is the name of a person.',
-      email: 'This is the email of a person.',
-      publications: 'Publications',
-      project: 'This is the project of a person.',
-      projects: [
-        {
-          title: 'First project title',
-          mainInfo: 'First project description',
-        },
-        {
-          title: 'Second project title',
-          mainInfo: 'Second project description',
-        },
-      ],
-      cvresume: 'Link to a CV',
-      awards: ['First award', 'Second Award'],
-      startYear: 2017,
-      studying: 'This is the studying of a person.',
-      pronouns: 'These are the pronouns of a person',
-      portraitLink: 'This is the portrait link of a person.'
+      portraitLink: 'This is the portrait link of a person.',
+      socialMedia: 'This is the social media of a person'.
     },
   ]];
 
