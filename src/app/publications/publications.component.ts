8d1a29b948d5fa41d06c3edc31ada99078152aaa
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-08-11 09:51:51 -0500

    Add publications and tweak interface based on experience

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Publication } from '../Classes/publication';
 import { PublicationsService } from './publications.service';
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-publications',
   templateUrl: './publications.component.html',
@@ -11,10 +11,14 @@ import { Observable } from 'rxjs';
 export class PublicationsComponent implements OnInit {
 
   publications$: Observable<Publication[]>;
+  MainAuthor = RegExp('Creanza N*')
+
   constructor(private publicationserv: PublicationsService) { }
 
   ngOnInit() {
-    this.publications$ = this.publicationserv.getPublications();
+    this.publications$ = this.publicationserv.getPublications().pipe(
+      map(pubs => pubs.sort((a,b) => (a.year > b.year ? -1 : a.year < b.year ? 1 : 0)))
+      );
   }
 
 }
