645017c0c6e7c35f5d985b62c736ec991bf1f5c9
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-08-12 06:15:55 -0500

    photos sorted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { Observable } from 'rxjs';
-
+import { map } from 'rxjs/operators';
+import { UploadPhoto } from '../Classes/uploadPhoto';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,12 +10,14 @@ import { Observable } from 'rxjs';
 })
 
 export class HomeComponent implements OnInit {
-  images$: Observable<string[]>;
+  images$: Observable<UploadPhoto[]>;
 
   constructor(private homeserv: HomeService) { }
 
   ngOnInit() {
-    this.images$ = this.homeserv.getImages();
+    this.images$ = this.homeserv.getImages().pipe(
+      map(photos => photos.sort((a,b) => a.date > b.date ? -1 : 1))
+    );
   }
 
 }
