f90de1b807760b5b778643c3502e6728589e197c
Cristina Robinson <NeuroBio@users.noreply.github.com>
2019-08-25 08:44:41 -0500

    fix people upload so all traits generated

--- src/app/Admin/Forms/new-memeber/new-memeber.component.ts
+++ src/app/Admin/Forms/new-memeber/new-memeber.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CRUDService } from '../crud.service';
 import { formatDate } from '@angular/common';
+import { Person } from 'src/app/Classes/person';
 
 @Component({
   selector: 'app-new-memeber',
@@ -20,10 +21,16 @@ export class NewMemeberComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit() {
-    const newPerson = Object.assign({}, this.newPersonForm.value);
-    newPerson.portraitLink = 'https://firebasestorage.googleapis.com/v0/b/creanza-lab-208216.appspot.com' +
+  onSubmit() {    
+    const blankPerson = new Person;
+    Object.keys(this.newPersonForm.controls).forEach(key => {
+      blankPerson[key] = this.newPersonForm.controls[key].value;
+    });
+
+    blankPerson.portraitLink = 'https://firebasestorage.googleapis.com/v0/b/creanza-lab-208216.appspot.com' +
                              '/o/Profiles%2FPlaceHolder.jpg?alt=media&token=21990311-773c-41ad-9949-c4a34812db2a';
+    console.log(blankPerson);
+    const newPerson = Object.assign({}, blankPerson);
     this.CRUD.uploadItem(newPerson, 'people').then(() => this.message = 'successful upload!');
   }
 
--- src/app/Classes/person.ts
+++ src/app/Classes/person.ts
@@ -4,19 +4,19 @@ export class Project {
 }
 
 export class Person {
-    email: string;
-    description: string;
-    endingYear: string;
-    name: string;
-    projectShort: string;
-    startYear: number;
-    studying: string;
-    pronouns: string;
-    portraitLink: string;
-    socialMedia: string; // should be a JSON string of Social Media
-    publications: string;  // should be a JSON string of Publication[]
-    projects: string;// should be a JSON string of Project[]
-    cvresume: string;// Link to cv
-    awards: string[];
-    pubName: string;
+    email: string = '';
+    description: string = '';
+    endingYear: string = '';
+    name: string = '';
+    projectShort: string = '';
+    startYear: string = '';
+    studying: string = '';
+    pronouns: string = '';
+    portraitLink: string = '';
+    socialMedia: string = ''; // should be a JSON string of Social Media
+    publications: string = '';  // should be a JSON string of Publication[]
+    projects: string = '';// should be a JSON string of Project[]
+    cvresume: string = '';// Link to cv
+    awards: string = '';//should be a JSON string of Award
+    pubName: string = '';
 }
